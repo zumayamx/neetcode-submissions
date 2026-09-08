fa6679f5ecea026e27dffe6f59e4493ff61cb38f
José Manuel García Zumaya <145822426+zumayamx@users.noreply.github.com>
2026-09-07 20:55:26 -0600

    Add: design-browser-history - submission-3

---
+++ Data Structures & Algorithms/design-browser-history/submission-3.cpp
@@ -0,0 +1,61 @@
+struct PageNode {
+    string url;
+    PageNode* prev = nullptr;
+    PageNode* next = nullptr;
+
+    PageNode(string x) { url = x; }
+};
+
+class BrowserHistory {
+    PageNode* currPage = nullptr;
+
+   private:
+    void deleteForward(PageNode* node) {
+        while (node) {
+            PageNode* next = node->next;
+            delete (node);
+            node = next;
+        }
+    }
+
+   public:
+    BrowserHistory(string homepage) {
+        PageNode* visitPage = new PageNode(homepage);
+        currPage = visitPage;
+    }
+
+    void visit(string url) {
+        PageNode* visitPage = new PageNode(url);
+
+        if (currPage->next) deleteForward(currPage->next);
+        currPage->next = visitPage;
+        currPage->next->prev = currPage;
+        currPage = visitPage;
+    }
+
+    string back(int steps) {
+        while (steps > 0 && currPage->prev) {
+            currPage = currPage->prev;
+            steps--;
+        }
+
+        return currPage->url;
+    }
+
+    string forward(int steps) {
+        while (steps > 0 && currPage->next) {
+            currPage = currPage->next;
+            steps--;
+        }
+
+        return currPage->url;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
